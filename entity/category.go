f13f26e7c424d0910320362be1e5017fb4edee14
hilmiikhsan <robbinhood22012@gmail.com>
2023-12-12 23:00:28 +0700

    feat: add module categories

--- entity/category.go
+++ entity/category.go
@@ -1,1 +1,47 @@
 package entity
+
+import (
+	"errors"
+
+	"github.com/ecommerce/dto"
+)
+
+var (
+	ErrCategoryNameIsRequired = errors.New("category name is required")
+)
+
+type Category struct {
+	ID        int    `db:"id"`
+	Name      string `db:"name"`
+	CreatedBy string `db:"created_by"`
+}
+
+func NewCategory() Category {
+	return Category{}
+}
+
+func (ca Category) Validate(req dto.CreateCategoryRequest, id string) (Category, error) {
+	if req.Name == "" {
+		return ca, ErrCategoryNameIsRequired
+	}
+
+	ca.Name = req.Name
+	ca.CreatedBy = id
+
+	return ca, nil
+}
+
+func (ca Category) CategoryResponse(categories []Category) []dto.GetListCategoryResponse {
+	responses := []dto.GetListCategoryResponse{}
+
+	for _, category := range categories {
+		response := dto.GetListCategoryResponse{
+			ID:   category.ID,
+			Name: category.Name,
+		}
+
+		responses = append(responses, response)
+	}
+
+	return responses
+}
