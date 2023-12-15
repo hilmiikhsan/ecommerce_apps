1c8ce4fc16842cd1f6b378f41afc508cb0323e22
hilmiikhsan <robbinhood22012@gmail.com>
2023-12-15 14:49:06 +0700

    feat: add unit test module categories

---
+++ domain/category/service_test.go
@@ -0,0 +1,142 @@
+package category
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/ecommerce/dto"
+	"github.com/ecommerce/entity"
+	"github.com/stretchr/testify/require"
+)
+
+var svc = CategoryService{}
+
+type mockCategoryRepository struct{}
+
+// Create implements Repository.
+func (mockCategoryRepository) Create(ctx context.Context, category entity.Category) (err error) {
+	return Create()
+}
+
+// GetAll implements Repository.
+func (mockCategoryRepository) GetAll(ctx context.Context) (categories []entity.Category, err error) {
+	return GetAll()
+}
+
+var (
+	Create func() (err error)
+	GetAll func() (categories []entity.Category, err error)
+)
+
+func init() {
+	mock := mockCategoryRepository{}
+
+	svc = NewCategoryService(mock)
+}
+
+func TestCreate(t *testing.T) {
+	type testCase struct {
+		title       string
+		expectedErr error
+		request     entity.Category
+		before      func()
+	}
+
+	var testCases = []testCase{
+		{
+			title:       "create category success",
+			expectedErr: nil,
+			request: entity.Category{
+				ID:   1,
+				Name: "category 1",
+			},
+			before: func() {
+				Create = func() (err error) {
+					return nil
+				}
+			},
+		},
+		{
+			title:       "create category failed internal server error",
+			expectedErr: errors.New("internal server error"),
+			request: entity.Category{
+				ID:   1,
+				Name: "category 1",
+			},
+			before: func() {
+				Create = func() (err error) {
+					return errors.New("internal server error")
+				}
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.title, func(t *testing.T) {
+			test.before()
+
+			err := svc.CreateCategory(context.Background(), test.request)
+			require.Equal(t, test.expectedErr, err)
+		})
+	}
+}
+
+func TestGetAll(t *testing.T) {
+	type testCase struct {
+		title         string
+		expectedErr   error
+		expectedValue []dto.GetListCategoryResponse
+		before        func()
+	}
+
+	var testCases = []testCase{
+		{
+			title:       "get all category success",
+			expectedErr: nil,
+			expectedValue: []dto.GetListCategoryResponse{
+				{
+					ID:   1,
+					Name: "category 1",
+				},
+				{
+					ID:   2,
+					Name: "category 2",
+				},
+			},
+			before: func() {
+				GetAll = func() (categories []entity.Category, err error) {
+					return []entity.Category{
+						{
+							ID:   1,
+							Name: "category 1",
+						},
+						{
+							ID:   2,
+							Name: "category 2",
+						},
+					}, nil
+				}
+			},
+		},
+		{
+			title:         "get all category failed internal server error",
+			expectedErr:   errors.New("internal server error"),
+			expectedValue: []dto.GetListCategoryResponse{},
+			before: func() {
+				GetAll = func() (categories []entity.Category, err error) {
+					return nil, errors.New("internal server error")
+				}
+			},
+		},
+	}
+
+	for _, test := range testCases {
+		t.Run(test.title, func(t *testing.T) {
+			test.before()
+
+			_, err := svc.GetListCategory(context.Background())
+			require.Equal(t, test.expectedErr, err)
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.2.0
 	github.com/sirupsen/logrus v1.9.3
+	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.7.0
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -16,12 +17,14 @@ require (
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/google/uuid v1.4.0 // indirect
 	github.com/klauspost/compress v1.16.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.50.0 // indirect
